5c243b97ec79e815e2dba5a8365aa92e6df80d2d
soaresandrie <jsoaresandrie@gmail.com>
2020-01-20 23:22:16 -0300

    Exercicio6 - Manipulando arquivos - DELETE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -32,4 +32,10 @@ describe("Suite de manipulacao de Herois", () => {
 
     deepEqual(actual, expected);
   });
+
+  it("deve remover um heroi por id", async () => {
+    const expected = true;
+    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    deepEqual(resultado, expected);
+  });
 });
