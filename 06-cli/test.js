74b02438d08a31dd8a309a02c1ed73ea671b4558
soaresandrie <jsoaresandrie@gmail.com>
2020-01-21 23:50:22 -0300

    Exercicio6 - Manipulando arquivos - UPDATE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -1,15 +1,23 @@
 const { deepEqual, ok } = require("assert");
 
 const database = require("./database");
+
 const DEFAULT_ITEM_CADASTRAR = {
   nome: "Flash",
   poder: "Speed",
   id: 1
 };
 
+const DEFAULT_ITEM_ATUALIZAR = {
+  noe: "Lanterna Verde",
+  poder: "Energia do Anel",
+  id: 2
+};
+
 describe("Suite de manipulacao de Herois", () => {
   before(async () => {
     await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    await database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
   });
 
   it("deve pesquisar um heroi usando arquivos", async () => {
@@ -36,6 +44,21 @@ describe("Suite de manipulacao de Herois", () => {
   it("deve remover um heroi por id", async () => {
     const expected = true;
     const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    deepEqual(resultado, expected);
+  });
+
+  it("deve atualizar um heroi pelo id", async () => {
+    const expected = {
+      ...DEFAULT_ITEM_ATUALIZAR,
+      nome: "Batman",
+      poder: "Dinheiro"
+    };
+    const novoDado = {
+      nome: "Batman",
+      poder: "Dinheiro"
+    };
+    await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado);
+    const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id);
     deepEqual(resultado, expected);
   });
 });
